83f540300945c62c62de25108a968678a6d906a8
eunseo0403 <eunseo505@naver.com>
2025-05-06 16:33:51 +0900

    feat: login 제작

---
+++ src/main/java/com/example/shoppingmall/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "users/login";
+    }
+}
